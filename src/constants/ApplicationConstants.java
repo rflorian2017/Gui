aef1ae615687570d78b7e249794c52fa1a4fe797
rflorian2017 <roby_florian@yahoo.com>
2019-04-07 09:12:36 +0300

    added new tab for create table and join

--- src/constants/ApplicationConstants.java
+++ src/constants/ApplicationConstants.java
@@ -3,4 +3,9 @@ package constants;
 public class ApplicationConstants {
     public static final String COMPANIES_DIRECTORY_NAME = "companies";
     public static final String EMPLOYEE_TABLE = "employees";
+    public static final String SQLITE = "jdbc:sqlite:c:\\Users\\Roby-L\\JAVATIM2.db";
+    public static final String MYSQL = "jdbc:mysql://localhost:3306/sakila";
+    public static boolean MYSQL_SELECTED = true;
+    public static final String USER = "roby";
+    public static final String PWD = "admin";
 }
